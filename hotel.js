186cc5560ef5fd9e506734ddf46952a7ddfbddf7
Davsua <davidsuarezcarvajal98@gmail.com>
2023-11-02 22:29:12 -0500

    rooms done

--- hotel.js
+++ hotel.js
@@ -3,51 +3,80 @@ import fs from 'fs/promises';
 import puppeteer from 'puppeteer';
 
 export async function OpenEachLink(hotels, page) {
-  /*const browser = await puppeteer.launch({
-    headless: false,
-    slowMo: 30,
-    devtools: true,
-    //userDataDir: './cache/like',
-    //headless: 'new',
-    //args: ['--headless=new'],
-  });*/
   let result = [];
 
   try {
     for (const hotel of hotels) {
-      const { url, title, rating, stars } = hotel;
+      const { url } = hotel;
       //const page = await browser.newPage();
       await page.goto(url, { waitUntil: 'domcontentloaded' });
+      //const loc = await page.url();
+      //console.log(loc);
 
-      // Realiza acciones adicionales en la página si es necesario
+      const getHotelInfo = await page.evaluate((hotel) => {
+        const ROOMS_NUMBER = document.querySelectorAll(
+          '[data-et-mouseenter="goal:hp_rt_hovering_room_name"]'
+        );
 
-      const getHotelInfo = await page.evaluate(
-        (title, rating) => {
-          /*let title = document.querySelector('h2').textContent.trim();
-        let rating = document
-          .querySelector('[data-testid="review-score-right-component"] > [aria-label*="Scored"]')
-          ?.textContent.trim();*/
+        /*const ROOMS_BOX =
+          ROOMS_NUMBER.parentElement.parentElement.parentElement.parentElement.querySelectorAll(
+            '.e2e-hprt-table-row'
+          );*/
 
-          const ROOMS_NUMBER = document.querySelectorAll(
-            '[data-et-mouseenter="goal:hp_rt_hovering_room_name"]'
-          );
+        console.log(ROOMS_NUMBER.length);
 
-          let rooms = [];
+        let rooms = [];
 
-          ROOMS_NUMBER.forEach((roomElement) => {
-            let room = {
-              name: roomElement.textContent.trim(),
-              // Puedes agregar más propiedades según lo necesario
-            };
+        ROOMS_NUMBER.forEach((roomElement) => {
+          //console.log('PASO');
+          let filter =
+            roomElement.parentElement.parentElement.parentElement.parentElement.querySelectorAll(
+              '.e2e-hprt-table-row'
+            );
+          //console.log('PASO');
+
+          filter.forEach((roomBox) => {
+            let room = {};
+            //console.log(algo.previousElementSibling?.querySelector('[data-et-mouseenter="goal:hp_rt_hovering_room_name"]') ? true : false)
+
+            /*if (
+              roomBox.previousElementSibling?.querySelector(
+                '[data-et-mouseenter="goal:hp_rt_hovering_room_name"]'
+              )
+            ) {
+              //console.log('paso')
+
+              /*name: roomBox.previousElementSibling
+                  .querySelector('[data-et-mouseenter="goal:hp_rt_hovering_room_name"]')
+                  .textContent.trim(),*/
+
+            /*} else {
+              /*name: roomBox.previousElementSibling.querySelector('td > div > div').textContent.trim(),*/
+
+            /*}*/
+            let ppnight = roomBox.querySelector('.hprt-table-cell-price');
+            room.price = ppnight.querySelector('div.bui-price-display__value').textContent.trim();
+            room.taxes = roomBox.querySelector('.prd-taxes-and-fees-under-price').textContent.trim();
+            room.ocupancy = roomBox.querySelector('.hprt-table-cell-occupancy').textContent.trim();
+            room.discount = roomBox
+              .querySelector('[data-component="deals-container"]')
+              ?.textContent.split('off')
+              .shift()
+              .trim();
+
             rooms.push(room);
           });
+        });
 
-          return { rooms, title, rating, stars };
-        },
-        title,
-        rating
-      );
-      console.log(getHotelInfo.rooms);
+        //const { url, ...restHotel } = hotel;
+        //! FILTRAR OBJ REPETIDOS+
+        const uniqueRooms = rooms.filter(function (i, pos) {
+          return rooms.indexOf(i) === pos;
+        });
+
+        return { hotel, uniqueRooms };
+      }, hotel);
+      console.log(getHotelInfo.uniqueRooms.length);
       result.push(getHotelInfo);
 
       //await page.close();
@@ -57,6 +86,9 @@ export async function OpenEachLink(hotels, page) {
     //await browser.close();
 
     console.log(result);
+
+    const jsonResult = JSON.stringify(result, null, 2);
+    await fs.writeFile('resultado.json', jsonResult);
   }
 }
 
--- index.js
+++ index.js
@@ -9,24 +9,41 @@ import { OpenEachLink } from './hotel.js';
 (async () => {
   const browser = await puppeteer.launch({
     headless: false,
-    //slowMo: 30,
-    devtools: true,
+    //slowMo: 300,
+    //devtools: true,
     //userDataDir: './cache/like',
     //headless: 'new',
     //args: ['--headless=new'],
+    defaultViewport: { width: 1280, height: 1024 },
+    timeout: 60000,
   });
   const page = await browser.newPage();
 
-  const dateIn = '2023-12-24'; //formato AAAA/MM/DD
-  const dateOut = '2023-12-25'; //formato AAAA/MM/DD
-  const place = 'elpoblado';
+  const dateIn = '2024-01-24'; //formato AAAA/MM/DD
+  const dateOut = '2024-01-25'; //formato AAAA/MM/DD
+  const place = 'El+Poblado';
   const distance = 3000;
+  const mainPage = `https://www.booking.com/searchresults.en-us.html?checkin=${dateIn}&checkout=${dateOut}&selected_currency=USD&ss=${place}&ssne=${place}&ssne_untouched=${place}&lang=en-us&sb=1&src_elem=sb&src=searchresults&dest_type=city&group_adults=1&no_rooms=1&group_children=0&sb_travel_purpose=leisure%27&nflt=distance%3D${distance}`;
 
   //! https://www.booking.com/searchresults.en-us.html?checkin=2023-12-24&checkout=$2023-12-25&selected_currency=USD&ss=elpoblado&ssne=elpoblado&ssne_untouched=elpoblado&lang=en-us&sb=1&src_elem=sb&src=searchresults&dest_type=city&group_adults=1&no_rooms=1&group_children=0&sb_travel_purpose=leisure%27&nflt=distance%3D$3000
 
+  // Maneja el evento 'request'
+  /*page.on('request', (request) => {
+    // Cancela la redirección
+    if (request.isNavigationRequest() && request.redirectChain().length > 0) {
+      console.log(request.url());
+      //request.abort();
+    } else {
+      request.continue();
+    }
+  });*/
+
   await page.goto(
-    `https://www.booking.com/searchresults.en-us.html?checkin=${dateIn}&checkout=${dateOut}&selected_currency=USD&ss=${place}&ssne=${place}&ssne_untouched=${place}&lang=en-us&sb=1&src_elem=sb&src=searchresults&dest_type=city&group_adults=1&no_rooms=1&group_children=0&sb_travel_purpose=leisure%27&nflt=distance%3D${distance}`
+    `https://www.booking.com/searchresults.html?ss=${place}&ssne=${place}&ssne_untouched=${place}&label=gen173nr-1FCAMoMkIIbWVkZWxsaW5IM1gEaDKIAQGYATG4ARfIAQzYAQHoAQH4AQKIAgGoAgO4ArXokKoGwAIB0gIkYzg0ZTlkNDItNDFmNC00M2IxLTkxMDQtNzViN2I0ZmMwZDgx2AIF4AIB&sid=52feeb8ab898d8596b879bcfea0d1bcb&aid=304142&lang=en-us&sb=1&src_elem=sb&src=searchresults&dest_id=4137&dest_type=district&checkin=${dateIn}&checkout=${dateOut}&group_adults=1&no_rooms=1&group_children=0&nflt=distance%3D${distance}&selected_currency=USD`
   );
+
+  const loc = await page.url();
+  console.log(loc);
 
   const hotelCard = await page.evaluate(() => {
     const cards = document.querySelectorAll('[data-testid="property-card"]');
@@ -36,12 +53,20 @@ import { OpenEachLink } from './hotel.js';
       let title = card.querySelector('h3')?.textContent.trim();
       let starsCount = card.querySelectorAll('[data-testid="rating-stars"] > span');
       let stars = starsCount.length;
-      let rating = card.querySelector('[aria-label*="Scored"]').textContent.trim();
+      let rating = card.querySelector('[aria-label*="Scored"]')?.textContent.trim();
+
+      let benefitsArr = [];
+      card.querySelectorAll('.ba51609c35 > li').forEach((ben) => {
+        benefitsArr.push(ben.textContent.trim());
+      });
+      let benefits = benefitsArr.length > 0 ? benefitsArr : null;
+
       hotelsArray.push({
         url,
         title,
         rating,
         stars,
+        benefits,
       });
     });
     return hotelsArray;
